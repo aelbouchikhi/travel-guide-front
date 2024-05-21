d0edabb21f3414efa4c5c93dd8bcc5149dca4c90
aelbouchikhi <elbouchikhicompte@gmail.com>
2024-05-21 15:04:09 +0100

    filter users with role to show role : user

--- src/Components/GetAllUsers.jsx
+++ src/Components/GetAllUsers.jsx
@@ -59,18 +59,21 @@ const GetAllUsers = ({ users, setUsers, showUsers }) => {
       {showUsers && (
         <div>
           {users &&
-            users.map((user) => (
-              <div key={user.id} className="p-4 sm:ml-64">
-                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                  <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                      {user.email}{" "}
-                      {/* Adjust this according to your user data */}
-                    </p>
+            users.map(
+              (user) =>
+                user.role === "user" && (
+                  <div key={user.id} className="p-4 sm:ml-64">
+                    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">
+                          {user.email}{" "}
+                          {/* Adjust this according to your user data */}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                )
+            )}
         </div>
       )}
     </>
